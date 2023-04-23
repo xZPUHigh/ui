3f5519464b7ac46ce43dd08afc9ed90c74abe45f
ZPU <93166306+xZPUHigh@users.noreply.github.com>
2023-04-23 12:13:04 +0700

    Update zpuhubUI.lua

--- zpuhubUI.lua
+++ zpuhubUI.lua
@@ -1095,21 +1095,6 @@ function utility.rgba(r, g, b, alpha)
 end
 
 local themes = {
-    ZPUHUBStyle = {
-        ["Accent"] = Color3.fromRGB(111, 0, 255),
-        ["Window Background"] = Color3.fromRGB(47, 0, 88),
-        ["Window Border"] = Color3.fromRGB(0, 0, 0),
-        ["Tab Background"] = Color3.fromRGB(41, 0, 81),
-        ["Tab Border"] = Color3.fromRGB(45, 45, 55),
-        ["Tab Toggle Background"] = Color3.fromRGB(28, 28, 32),
-        ["Section Background"] = Color3.fromRGB(18, 18, 22),
-        ["Section Border"] = Color3.fromRGB(35, 35, 45),
-        ["Text"] = Color3.fromRGB(180, 180, 190),
-        ["Disabled Text"] = Color3.fromRGB(100, 100, 110),
-        ["Object Background"] = Color3.fromRGB(25, 25, 29),
-        ["Object Border"] = Color3.fromRGB(35, 35, 39),
-        ["Dropdown Option Background"] = Color3.fromRGB(19, 19, 23)
-    },
     Midnight = {
         ["Accent"] = Color3.fromRGB(100, 59, 154),
         ["Window Background"] = Color3.fromRGB(30, 30, 36),
@@ -1124,6 +1109,21 @@ local themes = {
         ["Object Background"] = Color3.fromRGB(25, 25, 29),
         ["Object Border"] = Color3.fromRGB(35, 35, 39),
         ["Dropdown Option Background"] = Color3.fromRGB(19, 19, 23)
+    },
+    WhiteAndBlue = {
+        ["Accent"] = Color3.fromRGB(255,255,255),
+        ["Window Background"] = Color3.fromRGB(41, 41, 41),
+        ["Window Border"] = Color3.fromRGB(255, 255, 255),
+        ["Tab Background"] = Color3.fromRGB(34, 34, 34),
+        ["Tab Border"] = Color3.fromRGB(255, 255, 255),
+        ["Tab Toggle Background"] = Color3.fromRGB(92, 92, 92),
+        ["Section Background"] = Color3.fromRGB(0, 0, 0),
+        ["Section Border"] = Color3.fromRGB(0, 203, 255),
+        ["Text"] = Color3.fromRGB(255, 255, 255),
+        ["Disabled Text"] = Color3.fromRGB(0, 187, 255),
+        ["Object Background"] = Color3.fromRGB(41, 41, 41),
+        ["Object Border"] = Color3.fromRGB(85, 85, 85),
+        ["Dropdown Option Background"] = Color3.fromRGB(255, 255, 255)
     },
 }
 local themeobjects = {}
